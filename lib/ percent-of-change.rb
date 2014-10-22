aedc270ff0ef37b26a7def84e2c414d16a3f2f39
Zachary Perlmutter <zrp200@gmail.com>
2014-10-22 14:48:18 -0700

    Update  percent-of-change.rb

--- lib/ percent-of-change.rb
+++ lib/ percent-of-change.rb
@@ -1,2 +1,10 @@
-def percent-of-change; end
+def percent-of-change(original, new, info=Hash.new(false))
+  	value = new - original
+  	percentage = value / original
+  	percentage = percentage.to_i if info[:round]
+  	if info[:percent] then return "#{percentage*100}%"
+  	else
+  		return percentage
+  	end
+end
 alias poc percent-of-change
