eb53fd042bf7d741dc8e763999056f380d8d60ce
Zachary Perlmutter <zrp200@gmail.com>
2014-10-23 02:41:21 -0700

    Update  percent_of_change.rb

--- lib/ percent_of_change.rb
+++ lib/ percent_of_change.rb
@@ -1,5 +1,5 @@
-def percent_of_change(original, new, info=Hash.new(false))
-  	value = new - original
+def percent_of_change(original, newvalue, info=Hash.new(false))
+  	value = newvalue - original
   	percentage = value / original
   	percentage = percentage.to_i if info[:round]
   	if info[:percent] then return "#{percentage*100}%"
