6954c4912ad6f59baed06007037df06ef8ed1b5d
Zachary Perlmutter <zrp200@gmail.com>
2014-10-24 16:27:26 -0700

    Update Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,5 @@
 namespace 'travis' do
 	task :v000 do
-		`gem install percent-of-change-0.0.0.pre.alpha.#{ENV['TRAVIS_BUILD_NUMBER']}.gem`
+		puts `gem install percent-of-change-0.0.0.pre.alpha.#{ENV['TRAVIS_BUILD_NUMBER']}.gem`
 	end
 end
